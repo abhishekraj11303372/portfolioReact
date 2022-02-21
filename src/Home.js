730890efe0e1bf30f41697af2fd1a9d651398127
Abhishek Raj <abhishekraj1130@gmail.com>
2022-02-21 11:32:34 +0530

    css and image changes

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import './Home.css'
 import photo from './photo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab, fabrands ,faaccusoft } from "@fortawesome/free-brands-svg-icons"
+import { faCheckSquare, faCoffee , faEnvelope, faMailBulk, faSquare, faSquareEnvelope} from '@fortawesome/free-solid-svg-icons'
+library.add(fab, faCheckSquare, faCoffee,faMailBulk, faEnvelope)
 
 export default function Home() {
   return (
     <div className="container">
+        
+            
         <div className="left_container">
-
-             Hi, I am 
+           <div className='logo'><h1><FontAwesomeIcon icon="fa-brands fa-accusoft" /></h1></div>
+            <div className='intro'> Hi, I am
             <h1>Abhishek Raj</h1>
+                <div className='designation'>Senior Engineer - Product Development</div>
+                <div className='links'>
+                <FontAwesomeIcon icon="fa-solid fa-envelope" />
+                <FontAwesomeIcon icon="fa-brands fa-github" />
+                <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+                </div>
+            </div>
+            <div className='slant'>
+                hi
+            </div>
         </div>
         <div className='right_container'> 
-            <h3>About me Skills  Portfolio CONTACT ME</h3>
-            <img className='my_image' src={photo} />
+        
+            <div className='navbar' >
+                <button className='nav_a'>About me</button>
+                <button className='nav_a'>Skills</button>
+                <button className='nav_a'>Portfolio</button>
+                <button className='nav_b'>CONTACT ME</button>
+            </div>
+            <img className='my_image' src={photo} alt='My Photo'/>
+            
         </div>
   </div>
   )
