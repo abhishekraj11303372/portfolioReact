09186c05bc6111a6773a5674091158420859181c
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-09 14:20:52 +0530

    custom internationalization done

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React,{ Suspense, useState } from 'react';
+import i18n from "i18next";
+import { useTranslation, initReactI18next, Trans } from "react-i18next";
 import './Home.css'
 import photo from './photo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,22 +9,78 @@ import { fab, faFacebook } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 library.add(fab, faFacebook,faEnvelope)
 
+const translationsEn = {
+    welcome: "Hi , I am",
+    "sample-text": " <bold>Abhishek Raj</bold>",
+    changed: "Senior Engnieer - Product Development",
+    AboutButton: "About Me",
+    skillsButton:"Skills",
+    portfolioButton:"Portfolio",
+    contactButton:"Contact Me",
+    optionEn:"English",
+    optionJp:"Japanese",
+  };
+  
+  const translationsJp = {
+    welcome: "こんにちは、私は",
+    "sample-text": "<bold>アビシェク　レージ</bold>",
+    changed: "シニアエンジニア-製品開発",
+    aboutButton: "私について",
+    skillsButton:"スキル",
+    portfolioButton:"ポートフォリオ",
+    contactButton:"私に連絡して",
+    optionEn:"英語",
+    optionJp:"日本語",
+  };
+
+  i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: { translation: translationsEn },
+      jp: { translation: translationsJp },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+
+
 export default function Home() {
+
+    const { t } = useTranslation();
+  const [count, setCount] = useState(0);
+  const onChange = (event) => {
+    i18n.changeLanguage(event.target.value);
+    setCount((previousCount) => previousCount + 1);
+  };
+
   return (
+<Suspense fallback={"Loading..."}>
     <div className="container">
         
             
         <div className="left_container">
            <div className='logo'><h1><FontAwesomeIcon icon="fa-brands fa-accusoft" /></h1></div>
             <div className='intro'> 
-                <span>Hi, I am</span>
-            <h1>Abhishek Raj</h1>
-                <div className='designation'>Senior Engineer - Product Development</div>
+                <span>
+                {t("welcome")}
+                </span>
+                <br/>
+                <Trans components={{ bold: <strong />, italic: <i /> }}>
+              sample-text
+            </Trans>
+                
+                <div className='designation'><p>{t("changed")}</p></div>
                 <div className='links'>
                 <a href="mailto:abhishekraj1130@gmail.com"><FontAwesomeIcon icon="fa-solid fa-envelope" /></a>
                 <a href="https://github.com/abhishekraj11303372" target="_blank"><FontAwesomeIcon icon="fa-brands fa-github" /></a>
                 <a href="https://www.linkedin.com/in/abhishek-raj-08784350/" target="_blank"><FontAwesomeIcon icon="fa-brands fa-linkedin" /></a>
                 <a href="https://www.facebook.com/abhishekinvisible" target="_blank"><FontAwesomeIcon icon="fa-brands fa-facebook-square" /></a>
+                <select name="language" onChange={onChange}>
+            <option value="en">{t("optionEn")}</option>
+            <option value="jp">{t("optionJp")}</option>
+          </select>
                 </div>
             </div>
             <div className='slant'>
@@ -32,14 +90,15 @@ export default function Home() {
         <div className='right_container'> 
         
             <div className='navbar' >
-                <button className='nav_a'>About me</button>
-                <button className='nav_a'>Skills</button>
-                <button className='nav_a'>Portfolio</button>
-                <button className='nav_a'>Contact Me</button>
+                <button className='nav_a'>{t("aboutButton")}</button>
+                <button className='nav_a'>{t("skillsButton")}</button>
+                <button className='nav_a'>{t("portfolioButton")}</button>
+                <button className='nav_a'>{t("contactButton")}</button>
             </div>
             <img className='my_image' src={photo} alt='My Photo'/>
             
         </div>
   </div>
+</Suspense>
   )
 }
