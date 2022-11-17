df128f18e5cd1febf86f7ac1836228b63f8aeef5
Abhishek Raj <abhishekraj1130@gmail.com>
2022-11-17 23:08:07 +0530

    new image and summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
+import Home from './Components/Home/Home';
+import Summary from './Components/Summary/Summary';
 
 function App() {
   return (
     <div className="App">
        <Home/>
+       <Summary/>
     </div>
   );
 }
---
+++ src/Components/Summary/Summary.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Summary.css'
+
+export default function Summary() {
+  return (
+    <div className='desc'>
+      <div className='title'>Summary</div>
+      <div className='experience'>4 years of professional experience. Passionate to learn new technologies to improve my skill set and 
+knowledge to contribute to the overall growth of the organization and build my professional career.
+• Experience building PowerApps and Microsoft Flow using SharePoint list.
+• Experience in Building Enterprise using ASP.Net, SQL Server, Entity Framework.
+• Good Database Experience with MySQL, Oracle 10G.
+• Good Experience in client-side languages JavaScript.
+• Experience with Frontend technologies HTML 5, BOOTSTRAP and CSS.
+• Experience in managing the clients.
+• Experience in building Application using Agile Methodology.
+• Experience in maintenance involving enhancements, analysis, carrying out the change, Testing and 
+Deploying.
+• Requirement analysis, Problem Solving and Trouble Shooting. </div>
+    </div>
+  )
+}
