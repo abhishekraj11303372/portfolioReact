98a015434403ab38daa887c2b8695a0632b2d37d
Abhishek Raj <abhishekraj1130@gmail.com>
2022-11-19 12:17:31 +0530

    new sections

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
 import Summary from './Components/Summary/Summary';
+import Skills from './Components/Skill/Skills';
+import AboutMe from './Components/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App">
        <Home/>
        <Summary/>
+       <AboutMe/>
+       <Skills/>
     </div>
   );
 }
---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AboutMe.css'
+
+function AboutMe() {
+  return (
+    <div className='section'>
+        <div className='aboutme-section'>
+            <h3 className='heading'>ABOUT ME</h3><br/>
+            <p className='description'>I am always curious to learn more. Skilled in .NET Framework, C#, HTML,CSS, Core Java, Angular, Clojure, Microsoft Powerapps and Powerautomate(SharePoint). 
+            Learning Japanese</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default AboutMe
