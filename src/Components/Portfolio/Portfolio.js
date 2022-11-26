e34d1fdbd1408842d686e9dc804ae1a4cf6c4f66
Abhishek Raj <abhishekraj1130@gmail.com>
2022-11-27 01:38:52 +0530

    portfolio section images

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import './Portfolio.css'
 
 function Portfolio() {
   return (
     <div className='section'>
     <div className='aboutme-section'>
-        <h2 className='heading'>PORTFOLIO</h2><br/>
+        <div className='head'><h2 className='heading'>PORTFOLIO</h2></div>
+        
+        <img src={require('../../Assets/images/KantanAdmin.png')} alt='KantanAdmin'/>
+        <img src={require('../../Assets/images/FinancePOC.jpg')} alt='FinancePOC'/>
+        <img src={require('../../Assets/images/PortfolioJP.png')} alt='PortfolioJP'/>
+        <img src={require('../../Assets/images/PortfolioEng.png')} alt='PortfolioEng'/>
+        <img src={require('../../Assets/images/vr.png')} alt='vr'/>
+        <img src={require('../../Assets/images/CssEarth.png')} alt='CssEarth'/>
     </div>   
     </div>   
   )
